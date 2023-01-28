8d055fc8b0599602ad3944d73b4a9fffdb400b7f
emxd <emad.321a@gmail.com>
2023-01-28 22:37:27 +1100

    Complete rock-paper-scissors web app

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const playBtn = document.querySelector(".playBtn");
 const siteHeading = document.querySelector(".mainHeading");
 const descDiv = document.querySelector(".descMenu");
 const optionMenu = document.querySelector(".optionMenu");
+const scoreMenu = document.querySelector(".scoreMenu");
+const popup = document.querySelector(".popup");
+const result = document.querySelector(".gameResult");
+const playAgainBtn = document.querySelector(".playAgainBtn");
 
 const body = document.querySelector("body");
 const desc = document.createElement("h3");
@@ -10,11 +14,28 @@ const rockImg = document.createElement("img");
 const paperImg = document.createElement("img");
 const scissorsImg = document.createElement("img");
 
+const eventBoard = document.createElement("h3");
+const scoreBoard = document.createElement("h3");
+scoreBoard.classList.add("score");
+
+eventBoard.textContent = "Game Started! Pick Your option";
+scoreBoard.textContent = "0 - 0";
+
+eventBoard.setAttribute(
+  "style",
+  "text-align: center; font-size: 38px; color: #ef8354; margin-top: 30px"
+);
+
+scoreBoard.setAttribute(
+  "style",
+  "text-align: center; font-size: 48px; color: #BFC0C0; margin-top: 50px"
+);
+
 rockImg.src = "img/rock.png";
 paperImg.src = "img/file.png";
 scissorsImg.src = "img/scissors.png";
 
-desc.textContent = "10 Rounds, 3 Moves, Don't Lose!";
+desc.textContent = "5 Rounds, 3 Moves, Don't Lose!";
 desc.setAttribute(
   "style",
   "text-align: center; font-size: 28px; color: #BFC0C0"
@@ -30,9 +51,12 @@ playBtn.addEventListener("click", () => {
   optionMenu.appendChild(rockImg);
   optionMenu.appendChild(paperImg);
   optionMenu.appendChild(scissorsImg);
+  scoreMenu.appendChild(scoreBoard);
+  scoreMenu.appendChild(eventBoard);
 });
 
 const choicesArray = ["rock", "paper", "scissors"];
+
 let playerScore = 0,
   computerScore = 0;
 
@@ -41,12 +65,21 @@ let getComputerChoice = () => {
   return choicesArray[random];
 };
 
-let playGame = (playerSelection, computerSelection) => {
-  playerSelection = playerSelection.toLowerCase();
-  let string = null;
+rockImg.addEventListener("click", () => game("rock", getComputerChoice()));
+
+paperImg.addEventListener("click", () => game("paper", getComputerChoice()));
+
+scissorsImg.addEventListener("click", () => game("scissors", getComputerChoice()));
+
+playAgainBtn.addEventListener("click", () => {
+  popup.setAttribute("style", "visibility: hidden;");
+  eventBoard.textContent = "Let's go again! Pick an option";
+  scoreBoard.textContent = "0 - 0";
+})
+
+let playRound = (playerSelection, computerSelection) => {
   if (playerSelection == computerSelection) {
     string = "Draw || Both players played the same move!";
-    return string;
   } else {
     if (
       (playerSelection == "rock" && computerSelection == "scissors") ||
@@ -54,46 +87,32 @@ let playGame = (playerSelection, computerSelection) => {
       (playerSelection == "scissors" && computerSelection == "paper")
     ) {
       playerScore++;
-      string =
-        "Your " +
-        playerSelection +
-        " beats the computers " +
-        computerSelection +
-        "! Score " +
-        playerScore +
-        "--" +
-        computerScore;
-      return string;
+      eventBoard.textContent =
+        "Your " + playerSelection + " beats the computers " + computerSelection;
     } else {
       computerScore++;
-      string =
+      eventBoard.textContent =
         "Your " +
         playerSelection +
         " lost to the computers " +
-        computerSelection +
-        "! Score " +
-        playerScore +
-        "--" +
-        computerScore;
-      return string;
+        computerSelection;
     }
+    scoreBoard.textContent = playerScore + " - " + computerScore;
   }
 };
 
-// let game = () => {
-//     let playerSelection, computerSelection;
-//     for (let i = 0; i < 5; i++){
-//         playerSelection = prompt("Enter Rock, Paper or Scissors");
-//         computerSelection = getComputerChoice();
-//         console.log(playGame(playerSelection, computerSelection));
-//     }
-//     if(playerScore > computerScore)
-//     {
-//         console.log("You Won!");
-//     }
-//     else {
-//         console.log("you lost!");
-//     }
-// }
-
-game();
+let game = (playerSelection, computerSelection) => {
+  playRound(playerSelection, computerSelection);
+  if (playerScore + computerScore == 5) {
+    if (playerScore > computerScore) {
+      eventBoard.textContent = "You Win! Game has restarted!";
+      result.textContent = "You Won!";
+    } else {
+      eventBoard.textContent = "You Lost! Game has restarted!";
+      result.textContent = "You Lost!";
+    }
+    popup.setAttribute("style", "visibility: unset;")
+    playerScore = 0;
+    computerScore = 0;
+  }
+};
